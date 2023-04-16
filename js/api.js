549db5802be7be4a6fc89978cfe7c5de63ed3e7c
nhatminh1978 <38359524+nhatminh1978@users.noreply.github.com>
2023-04-17 04:05:35 +0700

    131
    ddad

--- js/api.js
+++ js/api.js
@@ -2,11 +2,11 @@ fetch('https://643bb2a070ea0e660298d52b.mockapi.io/api/v1/pizza')
       .then(response => response.json())
       .then((data) => {
 		console.log(data)
-		document.getElementById('h1-1').innerHTML = data[0].name
-		document.getElementById('h1-2').innerHTML = data[1].name
-		document.getElementById('h1-3').innerHTML = data[2].name
-		document.getElementById('p-1').innerHTML = data[0].ingredients
-		document.getElementById('p-2').innerHTML = data[1].ingredients
-		document.getElementById('p-3').innerHTML = data[2].ingredients
-
+		for(let i=0;i<4;i++){
+			// document.getElementById("h1").innerHTML = data[i].name
+			document.getElementById(`p-${i}`).innerHTML = data[i].ingredients
+			document.getElementById(`h1-${i}`).innerHTML = data[i].name	
+			document.getElementById(`img-${i}`).src = data[i].image
+		}
+		
 	})
