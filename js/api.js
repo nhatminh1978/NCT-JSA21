e17137e97e7767971c03c255162433fb43dd1bd6
nhatminh1978 <38359524+nhatminh1978@users.noreply.github.com>
2023-04-16 16:01:26 +0700

    1313da
    dadad

--- js/api.js
+++ js/api.js
@@ -1,14 +1,12 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a1161f2c11msh5d466b1aee60b35p1cf74ajsn437a3b5e66c8',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
-};
+fetch('https://643bb2a070ea0e660298d52b.mockapi.io/api/v1/pizza')
+      .then(response => response.json())
+      .then((data) => {
+		console.log(data)
+		document.getElementById('h1-1').innerHTML = data[0].name
+		document.getElementById('h1-2').innerHTML = data[1].name
+		document.getElementById('h1-3').innerHTML = data[2].name
+		document.getElementById('p-1').innerHTML = data[0].ingredients
+		document.getElementById('p-2').innerHTML = data[1].ingredients
+		document.getElementById('p-3').innerHTML = data[2].ingredients
 
-fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', options)
-	.then(response => response.json())
-	.then((data) => {
-        console.log(data)
-    })
-	.catch(err => console.error(err));
+	})
